356befaa57cd6b041dc98ce48c43af99256c7369
OllieClarke <43183262+OllieClarke@users.noreply.github.com>
2025-01-13 20:37:53 +0000

    Added logging to script

--- find-internet-speed.sh
+++ find-internet-speed.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+#make a timestamp command
+timestamp() {
+  date +%F_%T
+}
+
+timestamp #print timestamp
+#say where we're going
+echo changing directory to InternetSpeedChecker
 cd /home/pi/InternetSpeedChecker/
+
+timestamp #print timestamp
+#install requirements
+echo installing requirements
 /home/pi/.local/bin/pip3.12 install -r requirements.txt
+
+timestamp #print timestamp
+#run python script
+echo running python script
 /usr/local/bin/python3.12 script.py
