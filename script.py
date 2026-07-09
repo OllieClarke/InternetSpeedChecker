756b74757fbd6e61e8815ec274ce4d4cce5e7eba
OllieClarke <oliver.clarke@theinformationlab.co.uk>
2026-07-09 22:29:23 +0100

    updated python script to work with ookla cli

--- script.py
+++ script.py
@@ -1,51 +1,51 @@
 from dotenv import load_dotenv
 import os
+import sys
 import datetime
 import json
 import logging
 import boto3
 from botocore.exceptions import ClientError
 
-# Open the file in read mode and read the entire content as a string
+logging.basicConfig(level=logging.INFO)
+
 with open('speedoutput.txt', 'r') as file:
     content = file.read()
 
-# edit the json to remove server and client 
-# Convert string to dictionary
-data = json.loads(content) 
+if not content.strip():
+    logging.error("speedoutput.txt is empty - speedtest likely failed")
+    sys.exit(1)
 
-# Remove 'Client' info
-data.pop('Client', None)
+data = json.loads(content)
 
-# Rename keys to title case
-data = {key.title(): value for key, value in data.items()}
+# Strip identifying info 
+data.pop('interface', None)
+data.pop('isp', None)
 
-# Convert back to string
+# Convert bandwidth from bytes/sec to Mbps for continuity with old data
+data['download_mbps'] = round(data['download']['bandwidth'] * 8 / 1_000_000, 2)
+data['upload_mbps'] = round(data['upload']['bandwidth'] * 8 / 1_000_000, 2)
+data['ping_ms'] = data['ping']['latency']
+
 output = json.dumps(data, indent=4)
 
-#create timestamp
 timestamp = datetime.datetime.now().strftime('%Y%m%dT%H%M%S')
-
-#create output filename
 filename = f'speedoutput_{timestamp}.json'
 
-#Pull in values from secrets
 load_dotenv()
 ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
 ACCESS_SECRET = os.getenv("AWS_SECRET")
 BUCKET_NAME = os.getenv("AWS_BUCKET_NAME")
 
-#create a client authenticating with s3
 s3 = boto3.client(
     's3',
     aws_access_key_id=ACCESS_KEY,
     aws_secret_access_key=ACCESS_SECRET
-    )
+)
 
-#try and upload the json, log an error if not
 try:
-    #upload the json to s3
     response = s3.put_object(Bucket=BUCKET_NAME, Key=filename, Body=output)
-    print(response)
+    logging.info(f"Uploaded {filename}")
 except ClientError as e:
     logging.error(e)
+    sys.exit(1)
